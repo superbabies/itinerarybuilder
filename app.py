07dc1c5a407c79d979e2853f01080e63e2c41a8a
Madeleine Lerner <mlerner27@gmail.com>
2024-09-30 17:09:06 -0400

    cors

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, CORS
 
 from itinerary_builder import build_itinerary
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
